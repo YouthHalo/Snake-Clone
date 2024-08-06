2897f239e3daa9cb98c147653832c6d3504876dc
YouthHalo <69648246+YouthHalo@users.noreply.github.com>
2024-08-06 10:38:17 -0400

    Completed Snake Clone
    Added Apples to Eat, Death and Restart Mechanics

--- scenes/main.gd
+++ scenes/main.gd
@@ -9,6 +9,10 @@ var game_started : bool = false
 var cells : int = 20
 var cell_size : int = 50
 
+#Food Vars
+var food_pos : Vector2
+var regen_food : bool = true
+
 #Snake Vars
 var old_data : Array
 var snake_data : Array
@@ -28,11 +32,15 @@ func _ready():
 	new_game()
 
 func new_game():
+	get_tree().paused = false
+	get_tree().call_group("segments", "queue_free")
+	$GameOverMenu.hide()
 	score = 0
 	$"HUD/Score Label".text = "Score: " + str(score)
 	move_direction = up
 	can_move = true
 	generate_snake()
+	move_food()
 	
 func generate_snake():
 	old_data.clear()
@@ -40,7 +48,7 @@ func generate_snake():
 	snake.clear()
 	#Starting with start pos, create tail segments vertically down
 	for i in range(3):
-		add_segment(start_pos + Vector2(0, 1))
+		add_segment(start_pos + Vector2(0, i))
 	
 func add_segment(pos):
 	snake_data.append(pos)
@@ -49,6 +57,86 @@ func add_segment(pos):
 	add_child(SnakeSegment)
 	snake.append(SnakeSegment)
 	
+func move_snake():
+	if can_move:
+		#update movement from keypresses
+		if Input.is_action_just_pressed("move_up") and move_direction != down:
+			move_direction = up
+			can_move = false
+			if not game_started:
+				start_game()
+		if Input.is_action_just_pressed("move_down") and move_direction != up:
+			move_direction = down
+			can_move = false
+			if not game_started:
+				start_game()
+		if Input.is_action_just_pressed("move_left") and move_direction != right:
+			move_direction = left
+			can_move = false
+			if not game_started:
+				start_game()
+		if Input.is_action_just_pressed("move_right") and move_direction != left:
+			move_direction = right
+			can_move = false
+			if not game_started:
+				start_game()
+	
+func start_game():
+	game_started = true
+	$MoveTimer.start()
+
+func _on_move_timer_timeout():
+	can_move = true
+	#use snakes previous pos to move the segments
+	old_data = [] + snake_data
+	snake_data[0] += move_direction
+	for i in range(len(snake_data)):
+		#move all the segments along by one
+		if i > 0:
+			snake_data[i] = old_data[i - 1]
+		snake[i].position = (snake_data[i] * cell_size + Vector2(0, cell_size))
+	check_out_of_bounds()
+	check_self_eaten()
+	check_food_eaten()
+	
+func check_out_of_bounds():
+	if snake_data[0].x < 0 or snake_data[0].x > cells or snake_data[0].y < 0 or snake_data[0].y > cells-1:
+		end_game()
+
+func check_self_eaten():
+	for i in range(1, len(snake_data)):
+		if snake_data[0] == snake_data[i]:
+			end_game()
+
+func move_food():
+	while regen_food:
+		regen_food = false
+		food_pos = Vector2(randi_range(0, cells-1), randi_range(0, cells-1))
+		for i in snake_data:
+			if food_pos == i:
+				regen_food = true
+	$Food.position = (food_pos * cell_size) + Vector2(0, cell_size)
+	regen_food = true
+	
+func check_food_eaten():
+	if snake_data[0] == food_pos:
+		score += 1
+		$"HUD/Score Label".text = "Score: " + str(score)
+		add_segment(old_data[-1])
+		move_food()
+	
+
+func end_game():
+	$GameOverMenu.show()
+	$MoveTimer.stop()
+	game_started = false
+	get_tree().paused = true
+	
+func _on_game_over_menu_restart():
+	new_game()
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	move_snake()
+
+
